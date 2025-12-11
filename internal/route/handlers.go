05895c0a6e17275c772f072ec7fb712c71f7292f
Léo Gaiguant--Anquit <leo.gaiguantanquit@ynov.com>
2025-12-11 14:36:46 +0100

    feat(route): Ajout des logs pour le lancement du serveur dans handlers.go

--- internal/route/handlers.go
+++ internal/route/handlers.go
@@ -1,10 +1,17 @@
 package route
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func Launch() {
 	http.HandleFunc("/", home)
 
 	fs := http.FileServer(http.Dir("assets"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	fmt.Println("Server launch at : http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
