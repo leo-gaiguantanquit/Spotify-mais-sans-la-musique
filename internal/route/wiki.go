d7f8d14f1309176e5418b2d0f74c26ea1a2f981c
Léo Gaiguant--Anquit <leo.gaiguantanquit@ynov.com>
2025-12-11 15:12:40 +0100

    feat(route): Modification de la structure PageData et mise à jour des fonctions de rendu

--- internal/route/wiki.go
+++ internal/route/wiki.go
@@ -1,25 +1,44 @@
 package route
 
 import (
+	"bytes"
+	"fmt"
 	"net/http"
 	"text/template"
 )
 
 type PageData struct {
 	Title   string
-	Content *template.Template
+	Content string
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
+	data := HomeData{}
 
+	renderFile("Accueil", render("home", data), w)
 }
 
-func renderFile(title string, content *template.Template, w http.ResponseWriter) {
+func renderFile(title string, content string, w http.ResponseWriter) {
 	data := PageData{
 		Title:   title,
 		Content: content,
 	}
 
-	t, _ := template.ParseFiles("template/layout.html")
+	executeHTML("layout", data, w)
+}
+
+func executeHTML(fileName string, data any, w http.ResponseWriter) {
+	pathString := fmt.Sprintf("template/%v.html", fileName)
+
+	t, _ := template.ParseFiles(pathString)
 	t.Execute(w, data)
+}
+
+func render(fileName string, data any) string {
+	pathString := fmt.Sprintf("template/%v.html", fileName)
+
+	t, _ := template.ParseFiles(pathString)
+	var buf bytes.Buffer
+	t.Execute(&buf, data)
+	return buf.String()
 }
