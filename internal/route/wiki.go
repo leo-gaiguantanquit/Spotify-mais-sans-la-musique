3e12e4f271230440a987e89c2b14a564e7c8320b
Léo Gaiguant--Anquit <leo.gaiguantanquit@ynov.com>
2025-12-11 14:37:04 +0100

    feat(route): Ajout de la structure PageData et de la fonction renderFile dans wiki.go

--- internal/route/wiki.go
+++ internal/route/wiki.go
@@ -1,7 +1,25 @@
 package route
 
-import "net/http"
+import (
+	"net/http"
+	"text/template"
+)
+
+type PageData struct {
+	Title   string
+	Content *template.Template
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func renderFile(title string, content *template.Template, w http.ResponseWriter) {
+	data := PageData{
+		Title:   title,
+		Content: content,
+	}
+
+	t, _ := template.ParseFiles("template/layout.html")
+	t.Execute(w, data)
 }
