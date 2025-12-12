035b6ef2cdb5f5e0a4679ef796be58ef190ccac8
Léo Gaiguant--Anquit <leo.gaiguantanquit@ynov.com>
2025-12-12 16:29:25 +0100

    feat(utils): Ajout de la fonction LogError pour la gestion des erreurs dans les logs

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -9,3 +9,8 @@ func Log(info string) {
 	date := time.Now().Format("2006-01-02 15:04:05")
 	fmt.Printf("%v : %v\n", date, info)
 }
+
+func LogError(info string, err any) {
+	date := time.Now().Format("2006-01-02 15:04:05")
+	fmt.Printf("%v : %v : %v\n", date, info, err)
+}
