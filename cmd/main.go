8517d0d09bd59fc51af194504147482b6a5c8849
Léo Gaiguant--Anquit <leo.gaiguantanquit@ynov.com>
2025-12-12 16:28:57 +0100

    feat(api): Réorganiser l'initialisation de la configuration avant le lancement de l'API

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"SMSM/internal/api"
+	"SMSM/internal/config"
 	"SMSM/internal/route"
 )
 
 func main() {
-	route.Launch()
+	config.InitConfig()
 	api.Launch()
+	route.Launch()
 }
