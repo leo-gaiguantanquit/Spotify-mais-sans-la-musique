6831e552ed9a5f8756c6569cd3464fc193f6f6eb
Léo Gaiguant--Anquit <gaiguantleo@gmail.com>
2026-02-08 19:25:09 +0100

    feat: ajout des handlers pour la recherche d'artistes et leurs morceaux principaux dans l'API

--- internal/api/api.go
+++ internal/api/api.go
@@ -2,13 +2,73 @@ package api
 
 import (
 	"SMSM/pkg/utils"
+	"encoding/json"
+	"net/http"
 )
 
 func Launch() {
 	utils.Log("Chargement de l'API...")
 
 	InitSpotify()
-	// TODO: Ajouter les handlers nécéssaire pour js
 
-	utils.Log("API chargés avec succé")
+	// Enregistrement des handlers de l'API
+	http.HandleFunc("/api/search", handleSearch)
+	http.HandleFunc("/api/artist", handleArtist)
+	http.HandleFunc("/api/artist/top-tracks", handleArtistTopTracks)
+
+	utils.Log("API chargés avec succe")
+}
+
+func handleSearch(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query().Get("q")
+	if query == "" {
+		http.Error(w, "Query is required", http.StatusBadRequest)
+		return
+	}
+
+	result, err := Search(query)
+	if err != nil {
+		utils.LogError("API Search Error", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result)
+}
+
+func handleArtist(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		http.Error(w, "ID is required", http.StatusBadRequest)
+		return
+	}
+
+	artist, err := GetArtist(id)
+	if err != nil {
+		utils.LogError("API Artist Error", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(artist)
+}
+
+func handleArtistTopTracks(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		http.Error(w, "ID is required", http.StatusBadRequest)
+		return
+	}
+
+	tracks, err := GetArtistTopTrack(id)
+	if err != nil {
+		utils.LogError("API Artist Top Tracks Error", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tracks)
 }
